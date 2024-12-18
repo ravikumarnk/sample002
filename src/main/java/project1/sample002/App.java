07e8a15f4a6ca821f16468082e30c418cf92342d
ravikumar <ravikelakam@gmail.com>
2024-12-18 10:18:07 +0530

    first commit

---
+++ src/main/java/project1/sample002/App.java
@@ -0,0 +1,10 @@
+package project1.sample002;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/project1/sample002/AppTest.java
@@ -0,0 +1,20 @@
+package project1.sample002;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+    	System.out.println("running test script.. ");
+
+    }
+}
